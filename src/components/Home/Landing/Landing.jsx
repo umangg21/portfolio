d15561f4c21f4e14a00b98a07ee24c7a4c16ada3
Umang Gupta <umang@indwealth.in>
2020-05-17 16:52:42 +0530

    fix tagline

--- src/components/Home/Landing/Landing.jsx
+++ src/components/Home/Landing/Landing.jsx
@@ -18,7 +18,7 @@ export class Landing extends Component {
                 </div>
                 <div className="block-right landing-block-right text-white">
                     <div className="min-vh-70 content">
-                        <p className="mb-4">A frontend developer aiming to make tech easy and humane with the help of cool development.</p>
+                        <p className="mb-4">A frontend developer having 3+ years of experience in developing enterprise level web apps.</p>
                         <p>I am currently working as Frontend Developer at <b><i><a className="link" rel="noopener noreferrer" target="_blank" href="https://www.indmoney.com/">INDmoney</a></i></b>,
                         where we are building a wealth mangement platform where you can earn, save and track all your investments and liabilities.
                         </p>
