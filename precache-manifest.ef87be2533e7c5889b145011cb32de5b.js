a8d449a168ed10fdc9d70d21e2ac2dc2634ad04d
Umang Gupta <umang@indwealth.in>
2020-05-17 19:23:21 +0530

    Updates

--- precache-manifest.ef87be2533e7c5889b145011cb32de5b.js
+++ precache-manifest.ef87be2533e7c5889b145011cb32de5b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "20e34cc589bc99ae83cace7ca103eabb",
+    "revision": "97751b8c7a9508e9522fe9e552526cbc",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "6fbd3a9549190f28b45d",
-    "url": "/portfolio/static/css/main.7e6a49a6.chunk.css"
+    "revision": "69213ef9cda4ba6b00d7",
+    "url": "/portfolio/static/css/main.cd2c196e.chunk.css"
   },
   {
     "revision": "ad1a6646770a2d0e0c2e",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.01fd8806.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6fbd3a9549190f28b45d",
-    "url": "/portfolio/static/js/main.f8e8ce3c.chunk.js"
+    "revision": "69213ef9cda4ba6b00d7",
+    "url": "/portfolio/static/js/main.a0a224bf.chunk.js"
   },
   {
     "revision": "e5284505c87d0b24b753",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c8a2d835e2f90e880953a27b091e1d6c.js"
+  "/portfolio/precache-manifest.ef87be2533e7c5889b145011cb32de5b.js"
 );
 
 self.addEventListener('message', (event) => {
