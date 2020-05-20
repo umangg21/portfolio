dc7f881160ad82697653314135c2c2962c5c74fe
Umang Gupta <umang@indwealth.in>
2020-05-20 22:59:11 +0530

    add experience

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Landing from './components/Home/Landing/Landing';
 import LandingContact from './components/Home/LandingContact/LandingContact';
 import LandingCta from './components/Home/LandingCta/LandingCta';
 import ContactMe from './components/Home/ContactMe/ContactMe';
+import LandingExperience from './components/Home/LandingExperience/LandingExperience';
 
 function App() {
   return (<>
     <Landing />
     <LandingCta />
-    <LandingContact />
+    <LandingContact /> 
+    <LandingExperience />
     <ContactMe />
   </>
   );
